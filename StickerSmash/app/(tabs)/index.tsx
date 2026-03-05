a7a556dbe3d5d12a5d75f76c39783f005b7e6111
securitysteven <24697316+securitysteven@users.noreply.github.com>
2026-03-06 12:04:16 +1300

    Integrate image picker functionality into "Choose a photo" button in tabs index and improve code formatting consistency.

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
-import * as ImagePicker from 'expo-image-picker';
-
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-    const pickImageAsync = async () => {
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: true,
@@ -21,30 +20,31 @@ export default function Index() {
       alert('You did not select any image.');
     }
   };
-    return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <ImageViewer imgSource={PlaceholderImage}/>
-            </View>
-            <View style={styles.footerContainer}>
-                <Button theme="primary" label="Choose a photo"/>
-                <Button label="Use this photo"/>
-            </View>
-        </View>
-    );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        alignItems: 'center',
-    },
-    imageContainer: {
-        flex: 1,
-    },
-    footerContainer: {
-        flex: 1 / 3,
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
