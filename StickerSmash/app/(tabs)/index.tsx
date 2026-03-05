76046ebd6496aa1ec538ef91eccbf44633c52eee
securitysteven <24697316+securitysteven@users.noreply.github.com>
2026-03-06 12:18:25 +1300

    Add EmojiList integration: support emoji selection and update modal close functionality

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {View, StyleSheet} from 'react-native';
+import {ImageSourcePropType, View, StyleSheet} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import {useState} from 'react';
 
@@ -7,6 +7,7 @@ import ImageViewer from '@/components/ImageViewer';
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
 import EmojiPicker from '@/components/EmojiPicker';
+import EmojiList from '@/components/EmojiList';
 
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -15,6 +16,7 @@ export default function Index() {
     const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
     const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+    const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -67,7 +69,7 @@ export default function Index() {
                 </View>
             )}
             <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-                {/* Emoji list component will go here */}
+                <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
             </EmojiPicker>
         </View>
     );
