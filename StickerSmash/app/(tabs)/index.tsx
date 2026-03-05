4fcce00f78275f797753624da697bfa5d660c648
securitysteven <24697316+securitysteven@users.noreply.github.com>
2026-03-06 12:16:38 +1300

    Add modal visibility state and integrate EmojiPicker for sticker selection

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -4,9 +4,9 @@ import {useState} from 'react';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
-
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
 
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -14,6 +14,7 @@ const PlaceholderImage = require('@/assets/images/background-image.png');
 export default function Index() {
     const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
     const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -35,7 +36,11 @@ export default function Index() {
     };
 
     const onAddSticker = () => {
-        // we will implement this later
+        setIsModalVisible(true);
+    };
+
+    const onModalClose = () => {
+        setIsModalVisible(false);
     };
 
     const onSaveImageAsync = async () => {
@@ -61,6 +66,9 @@ export default function Index() {
                     <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
                 </View>
             )}
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+                {/* Emoji list component will go here */}
+            </EmojiPicker>
         </View>
     );
 }
